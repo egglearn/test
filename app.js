be4be822271749f602420a4c7f3ca3b6ab6cf976
marcus <marcushamlin1@gmail.com>
2021-05-15 12:19:21 +0200

    modulising

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 let first = document.getElementById("first");
 
 console.log(first);
+
+let data = [1, 2, 3];
+
+export { data };
--- second.js
+++ second.js
@@ -1,3 +1,7 @@
 let second = document.getElementById("second");
 
 console.log(second);
+
+import { data } from "app.js";
+
+console.log(data);
